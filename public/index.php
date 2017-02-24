3b96747d4001e11a7ab40940b032647dcb2716de
ww930\A631090 <mohd.mubeen@atos.net>
2017-02-24 16:18:28 +0530

    c

--- public/index.php
+++ public/index.php
@@ -1,4 +1,26 @@
 <?php
-$db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = umarora.cnwuurzv0sp4.us-west-2.rds.amazonaws.com)(PORT = 1521)))(CONNECT_DATA=(SID=XE)))";
-$c1 = oci_connect("oracle123","oracle123",$db);
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors',TRUE);
+
+$conn = oci_connect("oracle123","oracle123","//umarora.cnwuurzv0sp4.us-west-2.rds.amazonaws.com:1521/bptest");
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$stid = oci_parse($conn, 'select tablespace_name , table_name from all_table');
+oci_execute($stid);
+
+echo "<table border='1'>\n";
+while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "   <td>" . ($item !==null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>/n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
 ?>
+
