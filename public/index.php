bc99ce4f5cdae40b2ca9f13af698f7088fc0592c
ww930\A631090 <mohd.mubeen@atos.net>
2017-02-24 12:14:19 +0530

    c

---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+$db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = umarora.cnwuurzv0sp4.us-west-2.rds.amazonaws.com)(PORT = 1521)))(CONNECT_DATA=(SID=XE)))";
+$c1 = oci_connect("oracle123","oracle123",$db);
+?>
